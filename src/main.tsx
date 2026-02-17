8ce90a00198d2861ae336606e62f7d3517c486e0
OlexandrBilyk <0500423510s@gmail.com>
2026-02-17 14:28:55 +0200

    add base styles + normilize + reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
 import { BrowserRouter } from 'react-router-dom'
+
+import 'reset-css/reset.css';
+import 'modern-normalize/modern-normalize.css';
+import './sass/main.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
