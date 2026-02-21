e3097788f6e967e98156b0a6e87fee18abd172a4
vladyslavsheludko <vladsh2103@gmail.com>
2026-02-20 22:18:12 -0500

    Done api.ts to not call axios over and over again, created AuthSlice and made reducers for authorization component, created function for login

---
+++ src/redux/Auth/AuthSlice.ts
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { api } from "../../api/api";
+
+const initialState = {
+  user: null,
+  isLoading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    setUser(state, action) {
+      state.isLoading = false;
+      state.user = action.payload;
+      state.error = null;
+    },
+
+    setLoading(state) {
+      state.isLoading = true;
+      state.error = null;
+    },
+
+    setError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+
+    logoutUser(state) {
+      state.user = null;
+    },
+  },
+});
+
+export const { setUser, setLoading, setError, logoutUser } = authSlice.actions;
+export default authSlice.reducer;
+
+export const login = (email: string, password: string) => async (dispatch) => {
+  dispatch(setLoading());
+
+  try {
+    await api.post("auth/login", { email, password });
+
+    const response = await api.get("auth/users/me");
+
+    dispatch(setUser(response.data));
+  } catch (error) {
+    dispatch(setError("Невірний емейл або пароль"));
+  }
+};
