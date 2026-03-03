b72de7163e5b88666529f0e91cf9ba7eb3572013
OlexandrBilyk <0500423510s@gmail.com>
2026-03-03 17:45:37 +0200

    add 404

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,5 +1,22 @@
+import styles from '../../sass/components/NotFound/NotFound.module.scss'
+import { useNavigate } from "react-router-dom"
+import { Logo } from '../Header/Logo'
+
 export default function NotFound() {
-    return <>
-        <p>404</p>
-    </>
+    const navigate = useNavigate()
+
+    return (
+        <section className={styles.notFound}>
+            <div className={styles.thumb}>
+                <img src="/NotFound.png" alt="Not found" className={styles.img} />
+            </div>
+            <div className={styles.textBlock}>
+                <Logo></Logo>
+                <p className={styles.text}>404</p>
+                <p className={styles.text}>DIDN'T FIND ANYTHING HERE</p>
+            </div>
+
+            <button type="button" className={styles.button} onClick={() => navigate('/home')}>Назад на Головну</button>
+        </section>
+    )
 }
