647b70a651a1d70974eba94f76a3d3a12962db90
arhippticyn <kakashihatake6hokagegygg@gmail.com>
2026-03-08 20:10:22 +0200

    Write tablet adaptive

--- src/components/Expense/ExpenseList.tsx
+++ src/components/Expense/ExpenseList.tsx
@@ -4,12 +4,13 @@ import {
   useTypificatedSelector,
 } from '../../hooks/hooks'
 import { selectExpenses } from '../../redux/Expense/ExpireSelectors'
+import styles from '../../sass/components/Expense/Expense.module.scss'
 import {
   DeleteExpense,
   GetAllExpenses,
 } from '../../redux/Expense/ExpenseOperation'
 import { selectCategories } from '../../redux/Category/CategorySelectors'
-import { RiDeleteBin6Line } from 'react-icons/ri'
+import { AiFillDelete } from 'react-icons/ai'
 
 interface ExpenseListProps {}
 
@@ -27,36 +28,42 @@ const ExpenseList = ({}: ExpenseListProps) => {
   }
   return (
     <div>
-      {expenses.map(expense => {
-        return (
-          <table>
-            <tr>
-              <th>дата</th>
-              <th>опис</th>
-              <th>категорія</th>
-              <th>сума</th>
-              <th></th>
+      <table className={styles.expenseList}>
+        <tr className={styles.expenseHeadRows}>
+          <th>опис</th>
+          <th>дата</th>
+          <th>категорія</th>
+          <th>сума</th>
+          <th></th>
+        </tr>
+        {expenses.map(expense => {
+          return (
+            <tr key={expense.id} className={styles.expenseContent}>
+              <div className={styles.title}>
+                <div className={styles.info}>
+                  <td className={styles.category}>
+                    {
+                      categories.find(
+                        category => category.id === expense.category_id
+                      )?.name
+                    }
+                  </td>
+                  <td className={styles.date}>{expense.date}</td>
+                </div>
+                <td className={styles.descr}>{expense.description}</td>
+              </div>
+              <div className={styles.amount_info}>
+                <td className={styles.amountTxt}>-{expense.amount} грн</td>
+                <td >
+                  <button className={styles.btnDelete} onClick={() => handleDelete(expense.id)}>
+                    <AiFillDelete />
+                  </button>
+                </td>
+              </div>
             </tr>
-            <tr key={expense.id}>
-              <td>{expense.date}</td>
-              <td>{expense.description}</td>
-              <td>
-                {
-                  categories.find(
-                    category => category.id === expense.category_id
-                  )?.name
-                }
-              </td>
-              <td>{expense.amount}</td>
-              <td>
-                <button onClick={() => handleDelete(expense.id)}>
-                  <RiDeleteBin6Line />
-                </button>
-              </td>
-            </tr>
-          </table>
-        )
-      })}
+          )
+        })}
+      </table>
     </div>
   )
 }
