301b63489d21a0b0298efaa07742fce14e82419e
arhippticyn <kakashihatake6hokagegygg@gmail.com>
2026-03-09 23:38:23 +0200

    Edit styles in expense & add switcher in finance

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
 import Expense from '../components/Expense/Expense'
+import styles from '../sass/components/Expense/Expense.module.scss'
 import { Header } from '../components/Header/Header'
 
 export const Home = () => {
+  const [activePage, setActivePages] = useState(true)
   return (
     <div>
       <Header></Header>
-      <Expense></Expense>
+      {activePage ? <Expense></Expense> : null}
+
+      <div className={styles.switchBtns}>
+        <button
+          className={`${styles.BtnSwitch} ${activePage && styles.active}`}
+          onClick={() => setActivePages(true)}
+        >
+          Витрати
+        </button>
+        <button
+          className={`${styles.BtnSwitch} ${!activePage && styles.active}`}
+          onClick={() => setActivePages(false)}
+        >
+          Доходи
+        </button>
+      </div>
     </div>
-)
+  )
 }
