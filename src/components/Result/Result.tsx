6c3061b93f58891c4f0a8c8d415d37865312c6db
OlexandrBilyk <0500423510s@gmail.com>
2026-03-13 11:06:34 +0200

    fix styles

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -4,6 +4,7 @@ import {
 } from '../../hooks/hooks'
 import styles from '../../sass/components/Expense/Expense.module.scss'
 import { selectResult } from '../../redux/Finance/FinanceSelectors'
+import { selectFinances } from '../../redux/Finance/FinanceSelectors'
 import { useEffect } from 'react'
 import { GetResultFinance } from '../../redux/Finance/FinanceOperation'
 
@@ -14,22 +15,27 @@ interface ResultProps {
 const Result = ({ type }: ResultProps) => {
   const dispatch = useTypificatedDispatch()
   const results = useTypificatedSelector(selectResult)
+  const finances = useTypificatedSelector(selectFinances)
   const date = new Date()
 
   useEffect(() => {
     dispatch(GetResultFinance({ type: type, year: date.getFullYear() }))
-  }, [dispatch, type])
+  }, [dispatch, type, finances])
   return (
     <table className={styles.resultsTable}>
       <thead className={styles.resultsTableHead}>
-        <th className={styles.resultsTableHeadTitle}>зведення</th>
+        <tr>
+          <th className={styles.resultsTableHeadTitle} colSpan={2}>
+            зведення
+          </th>
+        </tr>
       </thead>
       <tbody className={styles.resultsTableBody}>
         {results.map(result => {
           return (
             <tr className={styles.resultsTableContainer} key={result.month}>
               <td className={styles.resultsTableContainerMonth}>{result.month}</td>
-              <td className={styles.resultsTableContainerTotal}>{result.total} грн</td>
+              <td className={styles.resultsTableContainerTotal}>{result.total.toFixed(2)} грн</td>
             </tr>
           )
         })}
